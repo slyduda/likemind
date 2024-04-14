458816671a1715a1276c2ac6d771c392d4f43488
Sylvester Duda <sduda@archer.com>
2024-04-14 12:20:41 -0700

    fix: added env to hashPassword util

--- utils/hashPassword.ts
+++ utils/hashPassword.ts
@@ -2,6 +2,6 @@ import { hashSync } from "bcrypt";
 
 // TODO: Find a way to do this asynchronously in our Valibot schema transformer
 export const hashPassword = (password: string): string => {
-  const hashedPassword = hashSync(password, "secret");
+  const hashedPassword = hashSync(password, process.env.BCRYPT_SALT);
   return hashedPassword;
 };
