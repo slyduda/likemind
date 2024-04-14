d9ef0f9f03905b6fed66897b687d61492b201f55
Sylvester Duda <sduda@archer.com>
2024-04-14 12:18:02 -0700

    refactor: removed zod and added valibot in env variable validation

--- env/index.ts
+++ env/index.ts
@@ -1,17 +1,18 @@
-import { z } from "zod";
+import { object, type Input, string, parse } from "valibot";
 
-export const env = z.object({
-  POSTGRES_HOST: z.string(),
-  POSTGRES_PORT: z.string(),
-  POSTGRES_USER: z.string(),
-  POSTGRES_PASSWORD: z.string(),
-  POSTGRES_DB: z.string(),
+export const envSchema = object({
+  POSTGRES_HOST: string(),
+  POSTGRES_PORT: string(),
+  POSTGRES_USER: string(),
+  POSTGRES_PASSWORD: string(),
+  POSTGRES_DB: string(),
+  BCRYPT_SALT: string()
 });
 
-env.parse(process.env);
+parse(envSchema, process.env);
 
 declare global {
   namespace NodeJS {
-    interface ProcessEnv extends z.infer<typeof env> {}
+    interface ProcessEnv extends Input<typeof envSchema> {}
   }
 }
