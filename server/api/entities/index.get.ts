ff2027633ddfd47efad9413d2ca49d436b87498b
Sylvester Duda <sduda@archer.com>
2024-04-14 12:17:17 -0700

    chore: cleaned up import

--- server/api/entities/index.get.ts
+++ server/api/entities/index.get.ts
@@ -1,4 +1,4 @@
-import { entityList } from "~/db/services/entity/entityList";
+import { entityList } from "@/db/services";
 
 export default defineEventHandler(
   async () => await entityList({ limit: 100, offset: 0 })
