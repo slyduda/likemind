8ec0e5fd7decf226d8a0fb7e33b989e6eda9ef93
Sylvester Duda <sduda@archer.com>
2024-04-14 22:19:40 -0700

    feat: added tailwind and prettier formatted

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="font-bold h-full">
     {{ data }}
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
   modules: ["@nuxt/test-utils/module"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
