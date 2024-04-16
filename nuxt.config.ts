8374a5f14fd6d9cf0c021c8e89c590e86a2a0cb5
Sylvester Duda <sduda@archer.com>
2024-04-15 20:51:55 -0700

    chore: installed vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/test-utils/module"],
+  modules: ["@nuxt/test-utils/module", "@vueuse/nuxt"],
   postcss: {
     plugins: {
       tailwindcss: {},
