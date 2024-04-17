0a571f3eb9fb2725e86dfac449f27dce23aefc50
Sylvester Duda <sduda@archer.com>
2024-04-16 22:10:22 -0700

    fix: removed vitest dotenv file

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,6 @@
 import { defineVitestConfig } from "@nuxt/test-utils/config";
+import "dotenv/config";
 
 export default defineVitestConfig({
   // any custom Vitest config you require
-  test: {
-    setupFiles: ["dotenv/config"],
-  },
 });
