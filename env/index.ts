235103c4af36b7baaab660c6121ce23a3d119881
Sylvester Duda <sduda@archer.com>
2024-04-16 21:54:17 -0700

    chore: new github actions

--- env/index.ts
+++ env/index.ts
@@ -8,6 +8,8 @@ import {
 } from "valibot";
 import "dotenv/config";
 
+console.log("DOES PROCESS EXIST?");
+console.log(Boolean(process.env.JWT_SECRET));
 export const envSchema = object({
   POSTGRES_HOST: string(),
   POSTGRES_PORT: transform(optional(string(), "5432"), (input) =>
