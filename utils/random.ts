3edfa8f02f424389fd0259719c7fa66e0cbce3e8
Sylvester Duda <sduda@archer.com>
2024-05-05 19:21:20 -0700

    feat: added checkbox and random id

--- utils/random.ts
+++ utils/random.ts
@@ -67,3 +67,14 @@ export const randomNonCollidingIndices = (
   }
   return indices;
 };
+
+export const randomId = (length: number): string => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let id = "";
+  for (let i = 0; i < length; i++) {
+    id += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return id;
+};
