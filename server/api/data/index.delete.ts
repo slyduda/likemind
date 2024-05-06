eb0139d2836a83b96b4720811dfeadd0d96fa3c2
Sylvester Duda <sduda@archer.com>
2024-05-05 19:08:09 -0700

    feat: added conditional clear demo

--- server/api/data/index.delete.ts
+++ server/api/data/index.delete.ts
@@ -1,6 +1,21 @@
+import { boolean, object, optional, parse } from "valibot";
 import { clearTables } from "~/db/services";
 
-export default defineEventHandler(async () => {
-  await clearTables();
+const clearSchema = object({
+  fake: optional(boolean(), true),
+});
+
+const deleteDataClearBodySchema = optional(clearSchema, {
+  fake: true,
+});
+
+export default defineEventHandler<
+  { body: typeof deleteDataClearBodySchema },
+  Promise<void>
+>(async (event) => {
+  const body = await readValidatedBody(event, async () =>
+    parse(deleteDataClearBodySchema, await readBody(event)),
+  );
+  await clearTables({ fake: body.fake });
   return;
 });
