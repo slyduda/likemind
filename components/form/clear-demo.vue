23497fe5741bea6c8edb77e0e7b5a3644a54872b
Sylvester Duda <sduda@archer.com>
2024-05-05 19:23:22 -0700

    feat: removed hard coded user from demo

--- components/form/clear-demo.vue
+++ components/form/clear-demo.vue
@@ -1,6 +1,9 @@
 <template>
   <form class="prose m-auto mb-12" @submit.prevent="clear">
     <h2 class="border-b pb-4">Clear Demo</h2>
+    <BaseCheckbox v-model="body.fake" class="mb-4"
+      >Clear Fake Data Only</BaseCheckbox
+    >
     <BaseButton class="mt-2 bg-red-500 text-red-50" :pending="pending">
       Clear Data
     </BaseButton>
@@ -15,11 +18,15 @@ definePageMeta({
 });
 
 const pending = ref(false);
+const body = reactive({
+  fake: true,
+});
 
 const clear = async () => {
   pending.value = true;
   await $fetch("/api/data", {
     method: "DELETE",
+    body,
     onResponseError,
     onResponse,
   });
